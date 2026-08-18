b8d055653e74908d9bf5a475fab644f2b85ed532
mrunmayeehaha <deoremrunmayee1@gmail.com>
2026-08-18 18:35:11 +0000

    product model and get product model api

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.json({ message: "Get products" });
-});
+const { getProducts } = require("../controllers/productController");
 
-router.post("/", (req, res) => {
-    res.json({ message: "Create product" });
-});
+router.get("/", getProducts);
 
 module.exports = router;
+
+
+
+
+
+
